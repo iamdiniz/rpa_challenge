e805516e1ce9afd4feb2dab45be0aaae123ead6b
iamdiniz <guilhermeplays03@gmail.com>
2024-10-24 11:18:59 -0300

    find element by XPath

--- rpa_challenge.py
+++ rpa_challenge.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://rpachallenge.com/")
+
+first_name = driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelFirstName']")
